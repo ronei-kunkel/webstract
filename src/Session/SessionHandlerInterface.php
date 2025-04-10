f2d4edab00e7bbcbbf4516be2ac09d7b80839135
Ronei Kunkel <ronei.kunkel@gmail.com>
2025-04-10 05:22:50 -0300

    Swap names between session and session handler interface

--- src/Session/SessionHandlerInterface.php
+++ src/Session/SessionHandlerInterface.php
@@ -5,17 +5,56 @@ declare(strict_types=1);
 namespace RoneiKunkel\Webstract\Session;
 
 use RoneiKunkel\Webstract\Session\Exception\SessionProviderUnreachableException;
+use RoneiKunkel\Webstract\Session\Exception\SessionValueNotFoundException;
 
 interface SessionHandlerInterface
 {
 	/**
+	 * @param SessionKeyInterface $key
+	 * @throws SessionProviderUnreachableException
+	 * @return mixed
+	 */
+	public function get(SessionKeyInterface $key): mixed;
+
+	/**
+	 * @param SessionKeyInterface $key
+	 * @param mixed $default
+	 * @throws SessionProviderUnreachableException
+	 * @return mixed
+	 */
+	public function getOrDefault(SessionKeyInterface $key, mixed $default = null): mixed;
+
+	/**
+	 * @param SessionKeyInterface $key
+	 * @param mixed $value
 	 * @throws SessionProviderUnreachableException
 	 * @return void
 	 */
-	public function create(): void;
+	public function set(SessionKeyInterface $key, mixed $value): void;
 
 	/**
+	 * @param SessionKeyInterface $key
+	 * @return bool
+	 */
+	public function has(SessionKeyInterface $key): bool;
+
+	/**
+	 * @param SessionKeyInterface $key
 	 * @return void
 	 */
-	public function destroy(): void;
+	public function delete(SessionKeyInterface $key): void;
+
+	/**
+	 * @param SessionKeyInterface $key
+	 * @throws SessionValueNotFoundException
+	 * @return mixed
+	 */
+	public function consume(SessionKeyInterface $key): mixed;
+
+	/**
+	 * @param SessionKeyInterface $key
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	public function consumeOrDefault(SessionKeyInterface $key, mixed $default = null): mixed;
 }
--- src/Session/SessionInterface.php
+++ src/Session/SessionInterface.php
@@ -5,56 +5,17 @@ declare(strict_types=1);
 namespace RoneiKunkel\Webstract\Session;
 
 use RoneiKunkel\Webstract\Session\Exception\SessionProviderUnreachableException;
-use RoneiKunkel\Webstract\Session\Exception\SessionValueNotFoundException;
 
 interface SessionInterface
 {
 	/**
-	 * @param SessionKeyInterface $key
-	 * @throws SessionProviderUnreachableException
-	 * @return mixed
-	 */
-	public function get(SessionKeyInterface $key): mixed;
-
-	/**
-	 * @param SessionKeyInterface $key
-	 * @param mixed $default
-	 * @throws SessionProviderUnreachableException
-	 * @return mixed
-	 */
-	public function getOrDefault(SessionKeyInterface $key, mixed $default = null): mixed;
-
-	/**
-	 * @param SessionKeyInterface $key
-	 * @param mixed $value
 	 * @throws SessionProviderUnreachableException
 	 * @return void
 	 */
-	public function set(SessionKeyInterface $key, mixed $value): void;
+	public function create(): void;
 
 	/**
-	 * @param SessionKeyInterface $key
-	 * @return bool
-	 */
-	public function has(SessionKeyInterface $key): bool;
-
-	/**
-	 * @param SessionKeyInterface $key
 	 * @return void
 	 */
-	public function delete(SessionKeyInterface $key): void;
-
-	/**
-	 * @param SessionKeyInterface $key
-	 * @throws SessionValueNotFoundException
-	 * @return mixed
-	 */
-	public function consume(SessionKeyInterface $key): mixed;
-
-	/**
-	 * @param SessionKeyInterface $key
-	 * @param mixed $default
-	 * @return mixed
-	 */
-	public function consumeOrDefault(SessionKeyInterface $key, mixed $default = null): mixed;
+	public function destroy(): void;
 }
